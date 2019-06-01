9342f7d51dbf64bb66dc509fc0f126096797c7d4
Chris Buchert <chris@smartystreets.com>
2019-05-31 19:10:55 -0600

    Removed a console log.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import content from "../content/md/index.md";
 
 export default function Index() {
-	console.log(content.html);
 	return (
 		<div dangerouslySetInnerHTML={{__html: content.html}}/>
 	);
