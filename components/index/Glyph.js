c15f1d642f504011895e4b6c6453ba9ebffcd7c7
Chris <christianbuchert@gmail.com>
2019-07-20 12:34:58 -0600

    Removed unused imports and props.

--- components/index/Glyph.js
+++ components/index/Glyph.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import ChrisGlyph from "../../static/img/chris-glyph.svg";
 
-export default function Glyph(props) {
+export default function Glyph() {
     return (
         <>
             <div className={"glyph--container"}>
