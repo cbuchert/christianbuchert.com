f75ad8f22ad3337d09e7cda6df8198a7d8c78d14
Chris <christianbuchert@gmail.com>
2019-07-20 11:18:32 -0600

    Started styling glyph.

--- components/index/Glyph.js
+++ components/index/Glyph.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ChrisGlyph from "../../static/img/chris-glyph.svg";
 
 export default function Glyph(props) {
     return (
         <>
             <div className={"glyph--container"}>
-                <ChrisGlyph/>
+                <ChrisGlyph
+                    id={"chris-glyph"}
+                    height={"100%"}
+                    width={"100%"}
+                    viewBox={"0 0 800 800"}
+                />
             </div>
             <style jsx>{`
                 .glyph--container {
