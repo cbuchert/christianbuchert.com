cc8b215bbcb3d928f1024483c99b6f4082b85792
Chris <christianbuchert@gmail.com>
2019-07-19 23:24:28 -0600

    Glyph SVG is displayed.

--- components/index/Glyph.js
+++ components/index/Glyph.js
@@ -1,7 +1,12 @@
+import React from "react";
+import ChrisGlyph from "../../static/img/chris-glyph.svg";
+
 export default function Glyph(props) {
     return (
         <>
-            <div className={"glyph--container"}>Glyph here.</div>
+            <div className={"glyph--container"}>
+                <ChrisGlyph/>
+            </div>
             <style jsx>{`
                 .glyph--container {
                     display: flex;
