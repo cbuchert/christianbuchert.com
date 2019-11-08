0bcfc4fa9ef46fdd868527bc1d863ae86939a63e
Chris <christianbuchert@gmail.com>
2019-11-07 19:08:15 -0700

    Blog posts have metadata.

--- content/md/blog/another-day-another-test.md
+++ content/md/blog/another-day-another-test.md
@@ -2,5 +2,6 @@
 date: 2019-11-06T23:19:02.589Z
 title: 'Another day, another test'
 description: In which Chris tests another feature.
+keywords: ["blog", "posts"]
 ---
 Whee! Are we there yet?
--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -7,14 +7,27 @@ export default class Post extends Component {
     console.log(query);
     const post = await import(`../../content/md/blog/${query.post}.md`);
 
-    return { ...post.default };
+    return { ...post.default, filename: query.post };
   }
 
   render() {
+    const baseUrl = "https://christianbuchert.com/blog";
+    const url = `${baseUrl}/${this.props.filename}.html`;
+
     return (
       <div>
         <Head>
           <title>{this.props.attributes.title}</title>
+          <meta charSet="UTF-8"/>
+          <meta name="description" content={this.props.attributes.description}/>
+          <meta name="keywords" content={this.props.attributes.keywords.join(",")}/>
+          <meta name="author" content="Christian Buchert"/>
+          <meta property="og:title" content={this.props.attributes.title}/>
+          <meta property="og:description" content={this.props.attributes.description}/>
+          <meta property="og:image" content={this.props.attributes.heroImage}/>
+          <meta property="og:url" content={url}/>
+          <meta name="twitter:card" content="summary_large_image"/>
+          <link rel="canonical" href={url}/>
         </Head>
 
         <h1>{this.props.attributes.title}</h1>
