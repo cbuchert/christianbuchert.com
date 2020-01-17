46a58d6818f49908c53971d60a473e4e5b8bff26
Chris <christianbuchert@gmail.com>
2020-01-16 17:14:52 -0700

    Commented out a couple meta properties that I'm not using yet.

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -20,11 +20,11 @@ export default class Post extends Component {
           <title>{this.props.attributes.title}</title>
           <meta charSet="UTF-8"/>
           <meta name="description" content={this.props.attributes.description}/>
-          <meta name="keywords" content={this.props.attributes.keywords.join(",")}/>
+          {/*<meta name="keywords" content={this.props.attributes.keywords.join(",")}/>*/}
           <meta name="author" content="Christian Buchert"/>
           <meta property="og:title" content={this.props.attributes.title}/>
           <meta property="og:description" content={this.props.attributes.description}/>
-          <meta property="og:image" content={this.props.attributes.heroImage}/>
+          {/*<meta property="og:image" content={this.props.attributes.heroImage}/>*/}
           <meta property="og:url" content={url}/>
           <meta name="twitter:card" content="summary_large_image"/>
           <link rel="canonical" href={url}/>
