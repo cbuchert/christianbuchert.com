0729f98c5e728305ab60216b9c2dcbb38c759eed
Chris <christianbuchert@gmail.com>
2023-03-30 15:10:43 -0600

    Added a fixed-width layout to the experiments page.

--- pages/experiments.tsx
+++ pages/experiments.tsx
@@ -1,14 +1,15 @@
 import type { NextPage } from "next"
 import Link from "next/link"
+import { FixedWidthLayout } from "../components/layouts/FixedWidthLayout"
 
 const Experiments: NextPage = (props) => {
-  return (<div>
+  return (<FixedWidthLayout>
     <h1>Experiments</h1>
     <ul>
       <li><Link href="/experiments/l-tree-grass">L-Tree Grass</Link></li>
       <li><Link href="/experiments/lorenz-attractor-2d">2D Lorenz Attractor</Link></li>
     </ul>
-  </div>)
+  </FixedWidthLayout>)
 }
 
 export default Experiments
