bb2a630cd4dd4e670e353f69300ff4d5e76e1f98
Chris <christianbuchert@gmail.com>
2023-03-30 15:26:40 -0600

    Experiments are rendered a little more nicely.

--- pages/experiments.tsx
+++ pages/experiments.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next"
 import Link from "next/link"
 import { FixedWidthLayout } from "../components/layouts/FixedWidthLayout"
+import styles from "../styles/experiments.module.css"
 
 const Experiments: NextPage = (props) => {
   return (<FixedWidthLayout>
-    <h1>Experiments</h1>
-    <ul>
+    <h1 className={styles.heading}>Experiments</h1>
+    <ul className={styles.links}>
       <li><Link href="/experiments/l-tree-grass">L-Tree Grass</Link></li>
       <li><Link href="/experiments/lorenz-attractor-2d">2D Lorenz Attractor</Link></li>
     </ul>
