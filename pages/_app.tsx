f2c9fba63d15a0ee18ab304bca87766abccc7686
Chris <christianbuchert@gmail.com>
2023-04-11 18:26:10 -0600

    Updated the page title. :|

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Chris Buchert</title>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+
+    <Component {...pageProps} />
+  </>
 }
+
 export default MyApp
